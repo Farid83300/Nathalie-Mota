64f38bdb544c417a3d4be87a56fe24ec91d200b4
Farid83300 <farid.zaffalone@gmail.com>
2025-04-01 11:29:13 +0200

    single.php et page.php ajouté

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+function charger_styles_theme() {
+    wp_enqueue_style('style-principal', get_stylesheet_uri(), array(), '1.0');
+    wp_enqueue_style('style-theme', get_template_directory_uri() . '/assets/css/theme.css', array(), '1.0');
+    wp_enqueue_style('style-modale', get_template_directory_uri() . '/assets/css/modale.css', array(), '1.0');
+}
+add_action('wp_enqueue_scripts', 'charger_styles_theme');
