c6429f40790a20e8d5c2693c53ae5e628fcefc0d
Farid83300 <farid.zaffalone@gmail.com>
2025-04-04 00:25:59 +0200

    rectification de la modale ok

--- template-parts/modale.php
+++ template-parts/modale.php
@@ -7,8 +7,8 @@
 ?>
 
 <!-- HTML pour la structure de la modale -->
-<div class="modal-contact" id="contactModal" style="display: none;">
-    <div class="modal-content" style="width: 597px; height: 769px; border-width: 5px;">
+<div class="modal-contact" id="contactModal">
+    <div class="modal-content">
         <!-- Bannière image en entête -->
         <div class="modal-header">
             <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/logo-modale.png' ); ?>" alt="Bannière de contact">
